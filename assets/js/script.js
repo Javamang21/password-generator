001cfabd7ae140629a5348ccb69d73af917f4393
Javamang21 <juliustovar96@gmail.com>
2021-07-11 16:05:30 -0400

    I needed to update the code and change the method I was using to create this password

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,38 @@
+
 // Assignment code here
 
-// DOM Elements
+// Variables with values, and variables we will use later. 
+
+var specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", " < ", "=", " > ", " ? ", "@", "[", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+var lowerCase = ["a", "b","c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+console.log(upperCase.length);
+
+// This is a function for each prompt
+
+function passwordGenerator() {
+    var passLength = prompt("Please choose a password that is no less than 8 characters and no more than 128 characters");
+        
+}
 
 
-var randomPasswordFunc = {
-    lower: randomLowerCase,
-    upper: randomUpperCase,
-    number: randomNumber,
-    symbol: randomSymbol
-  }
-  
-  
-  // Functions that write all lowercase, uppercase, symbols, numbers, then all the above or just selected.
-  
-  function randomLowerCase() {
-    var lowerResults = String.fromCharCode(Math.floor(Math.random() * 26) + 97); 
-    var confirmLower = window.confirm("Would you like to include lowercase letters in your password");
-    return confirmLower;
-  }
-  
-  function randomUpperCase() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-  }
-  
-  function randomNumber() {
-    return String.fromCharCode(Math.floor(Math.random() * 10)+ 48);
-  }
-  
-  function randomSymbol() {
-    var symbols = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-  
-    return symbols[Math.floor(Math.random()* symbols.length)];
-  }
-  
-  
-  // Get references to the #generate element
-  var generateBtn = document.querySelector("#generate");
-  
-  // Write password to the #password input
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-  
-    passwordText.value = password;
-  
-  }
-  
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
