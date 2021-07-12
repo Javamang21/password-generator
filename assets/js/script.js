2ea5be87e5822d6e42f36574372a6515d419c72f
Javamang21 <juliustovar96@gmail.com>
2021-07-11 20:33:00 -0400

    I wrapped the main ftunction that iterates the password and randomizes it over all the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,40 @@
+// Write password to the #password input
+function writePassword() {
+    var password = passwordGenerator();
+    var passwordText = document.querySelector("#password");
+    
+    passwordText.value = password;
+    
+  }
+
+  var generateBtn = document.querySelector("#generate");
+  
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
+
+// Function that iterates and prints the results
+
+function passwordGenerator() {
+    
+    var selectedCharacters = [];
+    var finalPassword = [];
+
 // Variables with values, and variables we will use later. 
 
-var specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", " < ", "=", " > ", " ? ", "@", "[", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+var specialCharacters = ["!", "#", "$", "%", "&", "*", "+", "-", "<", "=", ">", "?", "@", "^", "_", "`", "{", "|", "}", "~"];
 
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-var lowerCase = ["a", "b","c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-// This will include everything the user agrees to
-
-var selectedCharacters = [];
-
-//this is an empty array, in order to combine all the characters and mix them up for the randomized password
-var finalPassword = [];
-
 // List of all the password prompt questions for the user
-
-function passwordPrompts() {
-    var passLength = window.prompt ("Please choose a password that is no less than 8 characters and no more than 128 characters");
-        if (passLength < 8 || passLength > 128) {
-            window.alert ("Password length must be no less than 8 characters and no more than 128 characters, please try again!");
+    var passwordLength = window.prompt ("Please choose a password that is no less than 8 characters and no more than 128 characters");
+        if (passwordLength < 8 || passwordLength > 128) {
+            window.alert ("Password length must be a number that is no less than 8 characters and no more than 128 characters, please try again!");
+            return passwordGenerator();
         }
-            else if (passLength === '' || passLength instanceof String) { 
-                window.alert ("Password length must be a number between 8 and 128, please try again!");
-            }
-            else {
-                return passwordPrompts();
-            }
 // Confirm prompt for special characters
             var specialCharacterPrompt = window.confirm ("Would you like special characters in your password ?");
                 if (specialCharacterPrompt === true) {
@@ -57,7 +64,18 @@ function passwordPrompts() {
 
                 if (specialCharacterPrompt === false && numbersPrompt === false && lowerCasePrompt === false && upperCasePrompt === false) {
                     window.alert("In order to create your password you must select at least ONE character type !");
-
-                    return passwordPrompts();
+                    return passwordGenerator();
                 }
+        
+        for (var i = 0; i < passwordLength; i++) {
+            finalPassword = finalPassword + selectedCharacters[Math.floor(Math.random() * selectedCharacters.length)];
+          }
+        
+          return finalPassword;
+    
         }
+
+
+    
+
+
